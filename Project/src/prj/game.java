7717e0cd257faa2f4be47dd3e2898186fc9cd1fc
DavSCo <david.cohen@ynov.com>
2017-03-02 10:57:12 +0100

    delete jpp

--- Project/src/prj/game.java
+++ Project/src/prj/game.java
@@ -7,9 +7,9 @@ public class game {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Règles du jeu :\n"
-				+ "L'utilisateur doit déduire des quatres mot affiché l'idée ou le mot approprié dans un minimum de temps.\n"
-				+ "Pour donner le mot exact, il devra sélectionner parmi les lettres qui lui sont proposées.\n");
+		System.out.println("RÃ¨gles du jeu :\n"
+				+ "L'utilisateur doit dÃ©duire des quatres mot affichÃ© l'idÃ©e ou le mot appropriÃ© dans un minimum de temps.\n"
+				+ "Pour donner le mot exact, il devra sÃ©lectionner parmi les lettres qui lui sont proposÃ©es.\n");
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Commencer le jeu ? (O/N)");
 		String ouiOuNon = scan.nextLine();
@@ -33,32 +33,32 @@ public class game {
 				String[] alphabet = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","w","x","y","z"};
 						
 				String jeu[][] = { 
-						{ "Pomme", "Fraise", "Pêche", "Cerise", "Fruit", "5" },
-						{ "Epinard", "Carrotte", "Pomme de terre", "Courge", "Légume","6" },
+						{ "Pomme", "Fraise", "PÃ©che", "Cerise", "Fruit", "5" },
+						{ "Epinard", "Carrotte", "Pomme de terre", "Courge", "LÃ©gume","6" },
 						{ "France", "Russie", "Mexique", "Allemagne", "Pays", "4" },
 						{ "Vert", "Violet", "Orange", "Blanc", "Couleur", "7" },
-						{ "Tennis", "Natation", "Danse", "Athlétisme", "Sport", "5" },
-						{ "Lion", "Guépard", "Panterre", "Crocodile", "Animaux", "7" } };
+						{ "Tennis", "Natation", "Danse", "AthlÃ©tisme", "Sport", "5" },
+						{ "Lion", "GuÃ©pard", "Panterre", "Crocodile", "Animaux", "7" } };
 
 				String reponseJeu = jeu[choixJeu][4];
 				String lettreJeu = jeu[choixJeu][5];
 
 				System.out.println("\n1." + jeu[choixJeu][0] + " 2." + jeu[choixJeu][1] + " 3." + jeu[choixJeu][2]
 						+ " 4." + jeu[choixJeu][3]);
-				System.out.println("\nLe mot est constitué de " + lettreJeu +" parmis les lettre suivantes : ");
+				System.out.println("\nLe mot est constituÃ© de " + lettreJeu +" parmis les lettre suivantes : ");
 				System.out.println("Le mot est :");
 				String reponse = scan.nextLine();
 
 				if (reponse.equalsIgnoreCase(reponseJeu)) {
-					System.out.println("Bravo vous avez trouver la bonne réponse!\n");
+					System.out.println("Bravo vous avez trouver la bonne rÃ©ponse!\n");
 
 				} else {
 					String reponse2;
 					do {
-						System.out.println("Mauvaise réponse! \nLe mot est :");
+						System.out.println("Mauvaise rÃ©ponse! \nLe mot est :");
 						reponse2 = scan.nextLine();
 					} while (!reponse2.equalsIgnoreCase(reponseJeu));
-					System.out.println("Bravo vous avez trouver la bonne réponse!\n");
+					System.out.println("Bravo vous avez trouver la bonne rÃ©ponse!\n");
 				}
 
 				System.out.println("Voulez vous rejouez ? (O/N)");
